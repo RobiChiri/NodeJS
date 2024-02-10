301daf568b6673287bfeb13aabe2d7fc6b345210
Robichiri <robertochiriac8@gmail.com>
2024-02-10 09:44:50 +0100

    draw

---
+++ Draw.js
@@ -0,0 +1,24 @@
+function luckyDraw(player) {
+  return new Promise((resolve, reject) => {
+    const win = Boolean(Math.round(Math.random()));
+
+    process.nextTick(() => {
+      if (win) {
+        resolve(`${player} won a prize in the draw!`);
+      } else {
+        reject(new Error(`${player} lost the draw.`));
+      }
+    });
+  });
+}
+
+const players = ["Joe", "Caroline", "Sabrina"];
+
+let promiseChain = Promise.resolve();
+
+players.forEach((player) => {
+  promiseChain = promiseChain
+    .then(() => luckyDraw(player))
+    .then((result) => console.log(result))
+    .catch((error) => console.error(error.message));
+});
--- writeMessage.mjs
+++
@@ -1,9 +0,0 @@
-import { writeFile } from "node:fs";
-
-writeFile("message.txt", "Hello Node.js", "utf8", function (error) {
-  if (error) {
-    return console.log(error);
-  }
-
-  console.log("The file has been saved!");
-});
