911e6d88a2363a4a4a568693f75bc4f7510a16a9
Robichiri <robertochiriac8@gmail.com>
2024-02-14 08:46:38 +0100

    CRUD

--- src/server.ts
+++ src/server.ts
@@ -1,54 +1,65 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import joi from "joi";
 
-const router = express.Router();
-
-let planets: { id: number; name: string }[] = [
+interface Planet {
+  id: number;
+  name: string;
+}
+const planets: Planet[] = [
   { id: 1, name: "Earth" },
   { id: 2, name: "Mars" },
 ];
 
-router.get("/api/planets", (req, res) => {
+const planetSchema = joi.object({
+  id: joi.number().integer().required(),
+  name: joi.string().required(),
+});
+
+const app = express();
+
+app.get("/api/planets", (req: Request, res: Response) => {
   res.json(planets);
 });
 
-router.get("/api/planets/:id", (req, res) => {
-  const planet = planets.find(
-    (planet) => planet.id === parseInt(req.params.id)
-  );
-  if (!planet) {
-    return res.status(404).json({ msg: "Planet not found" });
-  }
+app.get("/api/planets/:id", (req: Request, res: Response) => {
+  const planet = planets.find((p) => p.id === parseInt(req.params.id));
+  if (!planet) return res.status(404).send("Planet not found");
   res.json(planet);
 });
 
-router.post("/api/planets", (req, res) => {
-  const newPlanet = {
+app.post("/api/planets", (req: Request, res: Response) => {
+  const { error } = planetSchema.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const planet = {
     id: planets.length + 1,
     name: req.body.name,
   };
-  planets.push(newPlanet);
+  planets.push(planet);
   res.status(201).json({ msg: "Planet created" });
 });
 
-router.put("/api/planets/:id", (req, res) => {
-  const planet = planets.find(
-    (planet) => planet.id === parseInt(req.params.id)
-  );
-  if (!planet) {
-    return res.status(404).json({ msg: "Planet not found" });
-  }
+app.put("/api/planets/:id", (req: Request, res: Response) => {
+  const planet = planets.find((p) => p.id === parseInt(req.params.id));
+  if (!planet) return res.status(404).send("Planet not found");
+
+  const { error } = planetSchema.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
   planet.name = req.body.name;
   res.json({ msg: "Planet updated" });
 });
 
-router.delete("/api/planets/:id", (req, res) => {
-  const planet = planets.find(
-    (planet) => planet.id === parseInt(req.params.id)
-  );
-  if (!planet) {
-    return res.status(404).json({ msg: "Planet not found" });
-  }
+app.delete("/api/planets/:id", (req: Request, res: Response) => {
+  const planet = planets.find((p) => p.id === parseInt(req.params.id));
+  if (!planet) return res.status(404).send("Planet not found.");
+
   const index = planets.indexOf(planet);
   planets.splice(index, 1);
   res.json({ msg: "Planet deleted" });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
